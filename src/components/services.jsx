16f886b98c44141130dc08cae10dbc55de57402c
radron1 <124513708+radron1@users.noreply.github.com>
2026-06-09 15:53:45 -0700

    Rename Services component to Project1

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const Services = (props) => {
+export const Project1 = (props) => {
   return (
-    <div id="services" className="text-center">
+    <div id="project1" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Our Services</h2>
+          <h2>Our Project 1</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
             dapibus leonec.
